48a0ef900998de1133d39c3d2bc8e412696c780c
Sina Pilehchiha <sina.pilehchiha@trailofbits.com>
2021-11-26 18:48:18 -0500

    beep.

--- contracts/tests/EchidnaTest.sol
+++ contracts/tests/EchidnaTest.sol
@@ -2,22 +2,22 @@
  * SPDX-License-Identifier:    MIT
  */
 
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.2;
 import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 import "@openzeppelin/contracts/proxy/utils/UUPSUpgradeable.sol";
 
 contract EchidnaTest is UUPSUpgradeable, Initializable {
+
     function upgradeTo(address newImplementation) external override onlyProxy {
         _authorizeUpgrade(newImplementation);
         _upgradeToAndCallSecure(newImplementation, new bytes(0), false);
     }
-    //function echidna_test() external pure returns (bool) {
-    //    return true;
-    //}
-    function _authorizeUpgrade(address) override {
-        //require(msg.sender != address(0));
-    }
+
     function echidna_test() external pure returns (bool) {
         return true;
+    }
+
+    function _authorizeUpgrade(address) internal view override {
+        require(msg.sender != address(0));
     }
 }
