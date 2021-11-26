90f6d3809057681c07dd16d567842f0734948589
Sina Pilehchiha <sina.pilehchiha@trailofbits.com>
2021-11-26 18:09:57 -0500

    beep.

--- contracts/tests/EchidnaERC1820.sol
+++ contracts/tests/EchidnaERC1820.sol
@@ -4,6 +4,7 @@
 
 pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/utils/introspection/ERC1820Implementer.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 
 contract EchidnaERC1820 is ERC1820Implementer {
     
