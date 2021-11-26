d98cfb06e2a016b5ade963688ee7711427781d29
Sina Pilehchiha <sina.pilehchiha@trailofbits.com>
2021-11-26 17:44:50 -0500

    beep.

--- contracts/tests/EchidnaTest.sol
+++ contracts/tests/EchidnaTest.sol
@@ -3,7 +3,7 @@
  */
 
 pragma solidity ^0.8.0;
-import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import "@openzeppelin/contracts-upgradeable/contracts/proxy/utils/Initializable.sol";
 import "@openzeppelin/contracts/proxy/utils/UUPSUpgradeable.sol";
 
 contract EchidnaTest is UUPSUpgradeable, Initializable {
